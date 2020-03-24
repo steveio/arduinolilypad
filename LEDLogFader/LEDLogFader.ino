17d24fae993e8e9afd51372d7f937f727d7ce008
steveio.com <info@steveio.com>
2020-03-24 22:30:40 +0000

    Added Blink fade class, step increment to increase effect rate

--- LEDLogFader/LEDLogFader.ino
+++ LEDLogFader/LEDLogFader.ino
@@ -37,14 +37,14 @@ public:
     led_(led), extra_delay_us_(extra_delay_us) {}
   /* fade-in */
   void in(void) {
-    for (int16_t i = 0; i < 255; ++i) {
+    for (int16_t i = 0; i < 255; (++i + step_increment)) {
       led_.setBrightness(i);
       sleep(i);
     }  
   }
   /* fade-out */
   void out(void) {
-    for (int16_t i = 255; i >= 0; --i) {
+    for (int16_t i = 255; i >= 0; (--i - step_increment)) {
       led_.setBrightness(i);
       sleep(i);
     }
@@ -52,8 +52,12 @@ public:
   void setExtraDelay(uint16_t d) {
     extra_delay_us_ = d;
   }
+  void setStepIncrement(uint16_t d) {
+    step_increment = d;
+  }
 private:
   BasicLed led_;
+  uint16_t step_increment = 0;
   uint16_t extra_delay_us_ = 0;
   
   void sleep(uint8_t i) {
@@ -62,8 +66,57 @@ private:
   }
 }; /* End of class LogarithmicFade */
 
-BasicLed led(9);
+class Blink {
+public:
+  Blink(const BasicLed& led, uint16_t extra_delay_us = 3000) : 
+    led_(led), extra_delay_us_(extra_delay_us) {}
+
+  void in(void) {
+    for (int16_t i = 0; i < 255; (++i + step_increment)) {
+      led_.setBrightness(i);
+      sleep(i);
+    }  
+    for (int16_t i = 0; i < 255; (++i + step_increment)) {
+      led_.setBrightness(i);
+      sleep(i);
+    }
+  }
+  void out(void) {
+    led_.setBrightness(0);
+  }
+  void setExtraDelay(uint16_t d) {
+    extra_delay_us_ = d;
+  }
+  void setStepIncrement(uint16_t d) {
+    step_increment = d;
+  }
+private:
+  BasicLed led_;
+  uint16_t step_increment = 0;
+  uint16_t extra_delay_us_ = 0;
+  
+  void sleep(uint8_t i) {
+    delayMicroseconds((uint16_t)(log(1. / (i + 1)) + 5.) * 1000);
+    delayMicroseconds(extra_delay_us_);
+  }
+}; /* End of class Blink */
+
+
+// RGB LED Red 9, Green 10, Blue 11
+BasicLed led(11);
 LogarithmicFade fade(led);
+
+BasicLed led2(10);
+LogarithmicFade fade2(led2);
+
+BasicLed led3(9);
+LogarithmicFade fade3(led3);
+
+// Pair of White LEDs
+BasicLed led4(6);
+//LogarithmicFade fade4(led4);
+Blink fade4(led4);
+
 
 void setup() {
   // do nothing
@@ -71,6 +124,22 @@ void setup() {
 
 void loop() {
   fade.setExtraDelay(random(0,1000));
+  fade2.setExtraDelay(random(0,2000));
+  fade3.setExtraDelay(random(0,3000));
+
+  fade4.setExtraDelay(random(0,5000));
+  fade4.setStepIncrement(random(0,125));
+
   fade.in();
+  fade2.in();
+  fade3.in();
+
+  fade4.in();
+
   fade.out();
+  fade2.out();
+  fade3.out();
+
+  fade4.out();
+
 }
